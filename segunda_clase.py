e0dcda13a8cfb3bfa6adb364712d7a5af43118d4
caro05y <carolina.yepesmon@gmail.com>
2025-02-03 15:38:58 -0500

    first commit

---
+++ segunda_clase.py
@@ -0,0 +1,14 @@
+numero = int(input("Ingrese un numero: "))
+
+if numero>0:
+    print("El numero es positivo")
+elif numero <0:
+    print("El numero es negativo")
+else:
+    print("El numero ingresado es 0")  
+          
+
+while  numero<=10:
+    print(numero)
+    numero +=5
+    
